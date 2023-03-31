8eb003132934f7dff35676e2fc3e5c37388476b3
Phuong vy <vy002359@gmail.com>
2023-04-01 01:17:12 +0700

    create database

--- DAO/connectDB.py
+++ DAO/connectDB.py
@@ -1,6 +1,52 @@
 import mysql.connector
 mydb = mysql.connector.connect(
-    host ='localhost',
-    user =' ',
-    password =''
+    host ="localhost",
+    user ="root",
+    password ="",
+    database ="studentmanager"
 )
+#print(mydb)
+#mydb.close()
+mycursor = mydb.cursor()
+'''
+mycursor.execute("""CREATE TABLE Khoi(
+                 idKhoi INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+                 tenKhoi VARCHAR(255) NOT NULL)""")'''
+'''mycursor.execute("""CREATE TABLE lop(
+                idLop INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+                tenLop VARCHAR(255) NOT NULL,
+                idKhoi INT not null,
+                FOREIGN KEY (idKhoi) REFERENCES Khoi (idKhoi)
+                            )""")'''
+'''mycursor.execute("""
+                CREATE TABLE Monhoc(
+                idMonHoc INT AUTO_INCREMENT PRIMARY KEY,
+                tenMonHoc VARCHAR(255) NOT NULL,
+                soTiet INT NOT NULL
+                                                )""")'''
+mycursor.execute("""
+                CREATE TABLE ChucVu(
+                idChucVu INT AUTO_INCREMENT PRIMARY KEY,
+                tenChucVu VARCHAR(255) NOT NULL
+                )
+                            """)
+mycursor.execute("""
+                CREATE TABLE CacKhoanPhi(
+                idCacKhoanPhi INT AUTO_INCREMENT PRIMARY KEY,
+                tenCacKhoanPhi VARCHAR(255) NOT NULL
+                )
+                    """)
+mycursor.execute("""
+                CREATE TABLE IF NOT EXISTS XEPLOAI(
+                idXepLoai INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                tenXepLoai VARCHAR(255) NOT NULL
+                )
+                """)
+mycursor.execute("""
+                CREATE TABLE IF NOT EXISTS QuyenHan(
+                idQuyenHan INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                tenQuyenHan VARCHAR(255) NOT NULL
+                )
+                    """)
+
+mydb.close()
